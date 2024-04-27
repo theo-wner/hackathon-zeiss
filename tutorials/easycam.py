6cc41aa310109d8c02b5c96428f74a00a3258398
DexBrox <jung.valentin98@gmail.com>
2024-04-27 13:55:52 +0200

    New File with easy pose-estimation

---
+++ tutorials/easycam.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+#from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("yolo-Weights/yolov8n-pose.pt")
+
+results = model.predict(source="0", show=True)
+
+print(results)
+
