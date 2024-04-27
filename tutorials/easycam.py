dd50856b74860f5d3916cb9b04475148ad1cb3a4
DexBrox <ujtfc@student.kit.edu>
2024-04-27 14:26:59 +0200

    poses xyn

--- tutorials/easycam.py
+++ tutorials/easycam.py
@@ -5,15 +5,18 @@ model = YOLO("yolo-Weights/yolov8n-pose.pt")
 # return a generator of Results objects
 results = model.predict(source="0", stream=True)
 
-
 for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
+    #boxes = result.boxes  # Boxes object for bounding box outputs
+    #masks = result.masks  # Masks object for segmentation masks outputs
     keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
+    #probs = result.probs  # Probs object for classification outputs
+    #obb = result.obb  # Oriented boxes object for OBB outputs
+    print(keypoints.xyn)
+
     result.show()  # display to screen
-    result.save(filename='result.jpg')  # save to disk
+
+
+
 
 
 
